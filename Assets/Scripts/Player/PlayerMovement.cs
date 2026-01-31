d75526a0ae14c6edcd516cbc6a266ec444ca8c37
김민우 <kmw6624@naver.com>
2026-01-31 21:56:27 +0900

    [Add] Player Movement with Input Actions
    - Implement player movement using Unity Input Actions
    - Add CharacterController-based movement logic
    - Bind Move action to player input for directional control

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 5.0f;
+    private float gravityValue = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    [Header("Input Actions")]
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerControls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        playerControls?.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls?.Disable();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer && playerVelocity.y < 0)
+            playerVelocity.y = 0f;
+
+        // Read input
+        float x = playerControls.Player.Move.ReadValue<Vector2>().x;
+        float y = playerControls.Player.Move.ReadValue<Vector2>().y;
+
+        Debug.Log(x + " , " + y);
+
+        Vector3 move = new Vector3(x, 0, y);
+        controller.Move(move * playerSpeed * Time.deltaTime);
+
+        if (move != Vector3.zero)
+            gameObject.transform.forward = move;
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
